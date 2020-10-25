9274651cada2f929aeb000b37720de65dafd29e2
AmmarCode <ammar.code01@gmail.com>
2020-10-25 12:34:30 -0700

    Updated code

--- starter_code/app.py
+++ starter_code/app.py
@@ -12,82 +12,13 @@ import dateutil.parser
 import psycopg2
 from flask import (Flask, Response, flash, redirect, render_template, request,
                    url_for)
-from flask_migrate import Migrate
-from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form
 
 from forms import *
-
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
-
-app = Flask(__name__)
-moment = Moment(app)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-
-# TODO: connect to a local postgresql database
-migrate = Migrate(app, db)
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
+from models import *
 
 
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    genres = db.Column(db.ARRAY(db.String))
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String(120))
-    show = db.relationship('Show', backref='Venue', lazy=True)
-
-    # TODO: implement any missing fields,
-    # as a database migration using Flask-Migrate
-
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String(120))
-    show = db.relationship('Show', backref='Artist', lazy=True)
-
-    # TODO: implement any missing fields,
-    # as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models,
-# and complete all model relationships and properties, as a database migration.
-
-
-class Show(db.Model):
-    __tablename__ = 'Show'
-
-    id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'),
-                         nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'),
-                          nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False)
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -196,12 +127,14 @@ def show_venue(venue_id):
     for show in shows:
         data = {
             "artist_id": show.artist_id,
-            "artist_name": show.artist_name,
-            "artist_image_link": show.artist_image_link,
+            "artist_name": show.artist.name,
+            "artist_image_link": show.artist.image_link,
             "start_time": format_datetime(str(show.start_time))
         }
         if show.start_time > current_time:
             upcoming_shows.append(data)
+        else:
+            past_shows.append(data)
 
     data = {
         "id": venue.id,
